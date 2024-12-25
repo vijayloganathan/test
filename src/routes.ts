1d0c79817473cebafb36989605c6e033771e3207
IONHAX04 <thirukumara04@gmail.com>
2024-12-25 11:17:24 +0530

    latest - drafted

--- src/routes.ts
+++ src/routes.ts
@@ -1,2 +1,9 @@
 import * as Hapi from "@hapi/hapi";
 
+import { UserRouters } from "./api/routes";
+
+export default class Router {
+  public static async loadRoutes(server: Hapi.Server): Promise<any> {
+    await new UserRouters().register(server);
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,48 @@
 const Hapi = require("@hapi/hapi");
 import logger from "./helper/logger";
 
+import Router from "./routes";
+
+import * as DotEnv from "dotenv";
+
+const init = async () => {
+  try {
+    DotEnv.config();
+
+    const server = Hapi.server({
+      host: process.env.HOST || "localhost",
+      port: process.env.PORT || 6201,
+      routes: {
+        cors: {
+          origin: ["*"],
+          headers: ["Accept", "Authorization", "Content-Type", "If-None-Match"],
+          exposedHeaders: ["WWW-Authenticate", "Server-Authorization"],
+          credentials: true,
+        },
+        security: true,
+        payload: {
+          maxBytes: 5242880,
+        },
+      },
+    });
+
+    await Router.loadRoutes(server);
+
+    await server.start((error: any) => {
+      if (error) {
+        logger.error(error);
+        throw error;
+      }
+    });
+    logger.info("Server running --- from server.ts", process.env.PORT);
+  } catch (error) {
+    logger.error("Server not running --- ", error);
+  }
+};
+
+process.on("unhandledRejection", (err) => {
+  console.log("Error", err);
+  process.exit(1);
+});
+
+init();
